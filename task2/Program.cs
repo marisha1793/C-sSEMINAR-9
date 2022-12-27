6a52f6263970049cd085b1313822c17ac6906bd9
Mariya <marisha_1793@mail.ru>
2022-12-28 01:55:03 +0700

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.WriteLine("Введите начальное число M:");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите следующее число диапазона N:");
+int N = int.Parse(Console.ReadLine());
+int sum = GetSumOfNumbersBetweenAB(M, N);
+Console.WriteLine($"Сумма чисел в промежутке от M до N равна {sum}");
+
+
+int GetSumOfNumbersBetweenAB(int M, int N)
+{
+    int sum = 0;
+    if (N < M)
+    {
+        return sum;
+    }
+    else
+    {
+        sum = sum + M;
+        M++;
+    }
+    return sum + GetSumOfNumbersBetweenAB(M,N);
+}
